b569781e383ffc2dab3d6302588df4e14ba567b0
renukadeshmukh <renud1988@gmail.com>
2015-05-04 01:35:04 -0400

    Added new files

--- WSDForSenseEval/WSDForSenseEval/Program.cs
+++ WSDForSenseEval/WSDForSenseEval/Program.cs
@@ -10,14 +10,24 @@ namespace WSDForSenseEval
     {
         static void Main(string[] args)
         {
+            GetDefaultSenseIdForEachWord();
+        }
+
+        public static void GenerateDefaultXML() {
             XMLParser xmlp = new XMLParser();
-           // xmlp.GenerateKeyXml();
+            // xmlp.GenerateKeyXml();
 
             //xmlp.CreateTrainFile();
-            
+
             xmlp.CreateTestFile();
 
             Console.WriteLine("Ineration finished !!");
+        }
+
+        public static void GetDefaultSenseIdForEachWord()
+        {
+            DefaultSenses ds = new DefaultSenses();
+            ds.GetDefaultSensesOfWord();
         }
     }
 }
